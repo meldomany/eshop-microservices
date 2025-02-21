ad18fdb005a6c7cd0322615f149d6a244821a658
Mohamed Abdou <meldomany04@gmail.com>
2025-02-21 14:02:27 +0200

    Add marten postgres configurations

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -7,7 +7,7 @@ namespace BuildingBlocks.CQRS
     {
     }
 
-    public interface ICommandHandler<in TCommand, TResponse> :IRequestHandler<TCommand, TResponse> 
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> 
         where TCommand : ICommand<TResponse>
         where TResponse : notnull
     {
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,18 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 // Services Injections
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 // Pipeline Configurations
