55791a2e9486772e8f37e3ddb459d7f4eb86a934
Mohamed Abdou <meldomany04@gmail.com>
2025-02-22 13:18:42 +0200

    Add get product by category handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryHandler(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByCategoryHandler, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryHandler request, CancellationToken cancellationToken)
+        {
+            var result = await session.Query<Product>().Where(e => e.Category.Contains(request.Category)).ToListAsync(cancellationToken);
+            return new GetProductByCategoryResult(result);
+        }
+    }
+}
