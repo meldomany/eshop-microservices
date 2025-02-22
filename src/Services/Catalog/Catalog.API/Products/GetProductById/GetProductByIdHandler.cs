458d2b1beee239de650e87cf0d2fcf5ee1a9bc87
Mohamed Abdou <meldomany04@gmail.com>
2025-02-22 12:52:56 +0200

    Add get product by id handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var result = await session.LoadAsync<Product>(query.Id);
+
+            if (result is null)
+                throw new ProductNotFoundException();
+
+            return new GetProductByIdResult(result);
+        }
+    }
+}
