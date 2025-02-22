2d2820d85b5c455318d70e301e3ec958532d52c9
Mohamed Abdou <meldomany04@gmail.com>
2025-02-22 13:13:58 +0200

    Add get product by id endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -6,7 +6,7 @@
     {
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            var result = await session.LoadAsync<Product>(query.Id);
+            var result = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
             if (result is null)
                 throw new ProductNotFoundException();
